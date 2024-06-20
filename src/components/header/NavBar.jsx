a60f6ea3b16a0b867d67c8cece7b3c282485fe4c
FernandoPusineri <fer.pusineri@gmail.com>
2024-06-21 01:19:58 +0200

    cambiosx2

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
         <nav className='nav'>
             <ul className='nav-menu'>
                 <li>
-                    <NavLink to="/" className='nav-link'>Inicio</NavLink>
+                    <NavLink to='/ThePhoneShop/' className='nav-link'>Inicio</NavLink>
                 </li>
                 {
                     clasificacion.map((categoria) => {
