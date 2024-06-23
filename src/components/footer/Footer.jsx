1269882988dca5f55ebd68b7c2e84ad5d4163578
FernandoPusineri <fer.pusineri@gmail.com>
2024-06-23 14:11:31 +0200

    modificamos el footer e instalamos firebase

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import tituloFooter from '../../../public/images/logo.png'
 
 export const Footer = () => {
   return (
     <footer className='footer'>
+      <div className='footer-contenedor'>
+      <Link to='/ThePhoneShop/' className='header-contenedor-footer'><img src={tituloFooter} className='titulo-footer' /></Link>
     <a href="https://github.com/FernandoPusineriWebMasterDesigner/ThePhoneShop" target="_blank">
               Hecho por <i className="fas fa-heart"></i> Fernando Pusineri WebMasterDesigner
             </a>
+            </div>
     </footer>
   )
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCxBJwBqDyRmTc7Oxs6av1YlsMDn8jpBww",
+    authDomain: "thephoneshop-6b6fd.firebaseapp.com",
+    projectId: "thephoneshop-6b6fd",
+    storageBucket: "thephoneshop-6b6fd.appspot.com",
+    messagingSenderId: "142529415236",
+    appId: "1:142529415236:web:2dbfb8aafb0704db6d5411"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
